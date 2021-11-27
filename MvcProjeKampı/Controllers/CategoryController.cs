a7e4430698e67d3ed579ccec7f3cc3becf78ec9d
SEMİH ÇAKMAK <semihcakmak7126@gmail.com>
2021-11-27 15:03:49 +0300

    deneme

--- MvcProjeKampı/Controllers/CategoryController.cs
+++ MvcProjeKampı/Controllers/CategoryController.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace MvcProjeKampı.Controllers
-{
+{   
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager cm = new CategoryManager();
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAllBL();
+            return View(categoryValues);
         }
     }
 }
